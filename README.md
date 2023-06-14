7ee9c267991cd1f96c329f45de57daa0b1324cca
Bissessar Kabir <bissesk1@l20-0572.org.aalto.fi>
2023-06-14 17:46:32 +0300

    changed some content on start screen, to test development configurations

---
+++ README.md
@@ -0,0 +1,3 @@
+NetDefender is a game designed for the course ELEC-C7420 Basic Principles in Networking at Aalto University.
+
+Developed by Kabir Bissessar and Maria Lappalainen.
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import hacker from './assets/undraw_hacker_mind_-6-y85.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -12,11 +12,9 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={hacker} className="logo react" alt="React logo" />
       </div>
-      <h1>Vite + React</h1>
+      <h1>NetDefender</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,7 +24,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Are you ready for the challenge?
       </p>
     </>
   )
