45e05398391bde046f213375feec444143e502af
Kabir Bissessar <kabir.bissessar@aalto.fi>
2023-06-15 19:54:12 +0300

    implemented chakra-ui on demo start screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import './App.css'
-import IntroScreen from './components/IntroScreen'
+import { ChakraProvider, extendTheme, AbsoluteCenter, Box } from "@chakra-ui/react";
+import IntroScreen from './components/IntroScreen';
+
+const theme = extendTheme({
+  colors: {
+    game: {
+      green: "#04A130",
+      black: "#0A0A0A",
+      white: "#F9F9F9",
+      red: "#C0181F"
+    }
+  },
+  styles: {
+    global: () => ({
+      body: {
+        bg: 'game.black',
+        color:'game.white'
+      }
+    })
+  }
+})
 
 const App = () => {
-  
+
   return (
-  <div>
-    <IntroScreen/>
-  </div>
+    <ChakraProvider theme = {theme}>
+      <Box pos='relative' w='100vw' minHeight='100vh'>
+        <AbsoluteCenter>
+          <IntroScreen/>
+        </AbsoluteCenter>
+      </Box>
+    </ChakraProvider>
   )
 }
 
--- src/components/IntroScreen.tsx
+++ src/components/IntroScreen.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import './intro.css';
+import { Box, Button } from "@chakra-ui/react";
 
 const IntroScreen = () => {
   const [displayText, setDisplayText] = useState("Welcome to NetDefender.");
@@ -8,23 +8,32 @@ const IntroScreen = () => {
   const addText = () => {
     setCount(count+1);
     if (count == 1) { 
-      setDisplayText("Your company has been hacked. <br /> IT has detected that the threat originated from your computer.");
+      setDisplayText("Your company has been hacked.");
     }
     if (count == 2) {
-      setDisplayText("But you are innocent.");
+      setDisplayText("IT has detected that the threat originated from your computer.");
     }
     if (count == 3) {
-      setDisplayText("It is up to you to save the company from the attack and prove your innocence. Enter the world of NetDefender.");
+      setDisplayText("But you are innocent.");
+    }
+    if (count == 4) {
+      setDisplayText("It is up to you to save the company from the attack and prove your innocence.");
+    }
+    if (count == 5) {
+      setDisplayText("Enter the world of NetDefender.");
     }
   }
 
-  return (  
-    <div className="intro_box">
-      <p>{displayText}</p>
+  return (
+    <Box border='2px' borderColor='whiteAlpha.800' boxSize='400px'>
+      <Box pos='absolute' top='0' left='0' marginTop='7em' marginLeft='3em' marginRight='3em' fontSize='20px' fontFamily='mono'>
+        <p>{displayText}</p>
+      </Box>
 
-      <button onClick={addText}> next </button>
-
-    </div>
+      <Button onClick={addText} position='absolute' bottom='0' left='0' marginBottom='3em' marginLeft='10em' bg='blackAlpha.900' border='2px' borderColor='whiteAlpha.800' color='whiteAlpha.800'> 
+        next
+      </Button>
+    </Box>
   );
 };
 
