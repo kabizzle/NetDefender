1a5ceb13b21215e15b4de7afbe3c7f59d4bdb4f0
Kabir Bissessar <kabir.bissessar@aalto.fi>
2023-06-26 18:03:12 +0300

    updated text, styled arrows and removed borders from Tutorial

--- src/components/IntroScreen.tsx
+++ src/components/IntroScreen.tsx
@@ -7,6 +7,7 @@ interface introProps{
   arrowProps: string[];
   handleClick(): void;
 }
+
 const IntroScreen = ({ displayText, arrowProps, handleClick }: introProps) => {
 
   return (
@@ -15,17 +16,17 @@ const IntroScreen = ({ displayText, arrowProps, handleClick }: introProps) => {
         <Image src="/arrow.svg" w={arrowProps[2]}/>
       </Box>
       <Box pos="relative" border='2px' borderColor='game.white' w='500px' h="350px">
-        <Center w="36px" h="36px" border="2px" borderColor="game.white" pos="absolute" top="0" right="0" m="1em 1em 0">
+        <Center w="36px" h="36px" border="2px" borderColor="game.white" pos="absolute" top="0" right="0" m="1em 1em 0" _hover={{color:"game.black", bg:"game.white"}}>
           <Link to="/">
             <CloseIcon />
           </Link>
         </Center>
-        <Box pos='absolute' m="6em 3em 0 3em" fontSize='20px' >
+        <Box pos='absolute' m="4em 3em 0 3em" fontSize='20px' >
           <p>
             {displayText}
           </p>
         </Box>
-        <Button onClick={handleClick} pos='absolute' m="16em 2.5em 0 13em" bg='game.black' border='2px' borderColor='whiteAlpha.800' color='whiteAlpha.800'> 
+        <Button onClick={handleClick} pos='absolute' m="16em 2.5em 0 13em" bg='game.black' border='2px' borderColor="game.white" color="game.white" _hover={{color:"game.black", bg:"game.white"}}> 
           next
         </Button>
       </Box>
--- src/pages/Tutorial.tsx
+++ src/pages/Tutorial.tsx
@@ -10,51 +10,52 @@ const Tutorial = () => {
   const [displayText, setDisplayText] = useState("Welcome to NetDefender.");
   const [count, setCount] = useState(0);
   const [arrowProps, setArrowProps] = useState(["", "", "0"]);
-  const [folderBorder, setFolderBorder] = useState("game.red");
-  const [progressBorder, setProgressBorder] = useState("game.white");
-  const [notificationBorder, setNotificationBorder] = useState("game.white");
 
   const handleClick = () => {
     setCount(count+1);
-    if (count == 1) { 
-      setDisplayText("Your company has been hacked.");
+    if (count === 0) { 
+      setDisplayText("Your company is being hacked and IT found that the attack started from your computer.");
+    }
+    if (count === 1) { 
+      setDisplayText("This is your progress section. Here, you can see your completion of the game, along with your current rating.");
       setArrowProps(["10em 0 0 20em", "rotate(0.6turn)", "7.5em"])
     }
-    if (count == 2) {
-      setDisplayText("IT has detected that the threat originated from your computer.");
-      setFolderBorder("game.red")
+    else if (count === 2) {
+      setDisplayText("Here you can find your messages. Be sure to check new messages, as there may be hidden clues :)");
+      setArrowProps(["12em 0 0 82em", "rotate(0.875turn)", "7.5em"])
+    }
+    else if (count === 3) {
+      setDisplayText("These are the level folders. Each folder contains the activities for one week.");
       setArrowProps(["40em 0 0 80em", "rotate(0.35turn)", "7.5em"])
     }
-    if (count == 3) {
-      setDisplayText("But you are innocent.");
+    else if (count === 4) {
+      setDisplayText("It is up to you to stop the attack and prove your innocence.");
+      setArrowProps(["", "", "0"])
     }
-    if (count == 4) {
-      setDisplayText("It is up to you to save the company from the attack and prove your innocence.");
-    }
-    if (count == 5) {
+    else if (count === 5) {
       setDisplayText("Enter the world of NetDefender.");
     }
   }
 
     return (
       <>  
-        <Grid templateRows="1fr 3 fr 1 fr" templateColumns="repeat(2, 1 fr)" h="100vh">
-          <GridItem colSpan={1} border="1px" borderColor="game.red">
-            <Box border="1px" borderColor={progressBorder} maxW="25em" p="0 0 2em 0">
+        <Grid templateRows="1fr 3fr 1fr" templateColumns="25em auto 25em" h="100vh">
+          <GridItem colSpan={1}>
+            <Box maxW="25em" p="0 0 2em 0">
               <UserProgress name={"Neo"} completed={1} rating={4}/>
             </Box>
           </GridItem>
-          <GridItem colSpan={1} border="1px" borderColor={notificationBorder}>
-          <Box pos="absolute" top="2em" right="2em" border="1px" borderColor="game.red" p="0 0 2em 0">
+          <GridItem colSpan={1} colStart={3}>
+            <Box pos="absolute" top="2em" right="2em" p="0 0 2em 0">
               <Notification containsMessages={true} nextMessage="/tutorial" />
             </Box>
           </GridItem>
-          <GridItem colSpan={2} border="1px" borderColor="game.gray">
+          <GridItem colSpan={3}>
             <Center m="5em 0 0 0">
               <IntroScreen displayText={displayText} arrowProps={arrowProps} handleClick={handleClick}/>
             </Center>
           </GridItem>
-          <GridItem rowSpan={1} colSpan={2}  padding="3em 10em 2em 10em" border="1px" borderColor={folderBorder}>
+          <GridItem rowSpan={1} colSpan={3}  padding="3em 10em 2em 10em">
             <Flex align="center" justify="space-between">
               <Box>
                 <Image src={"/folder_unlocked.svg"} w="10em"/>
