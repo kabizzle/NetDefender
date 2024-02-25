d1725dfc636385474cd498ae5219e7c1adafbe53
kabizzle <kabirrbissessar@gmail.com>
2024-02-25 18:15:02 +0200

    started styling Flashcard, it can now cycle through array of flashcards using button

--- src/components/Flashcards/Flashcard.tsx
+++ src/components/Flashcards/Flashcard.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardBody, CardHeader, Center, Heading, Text } from '@chakra-ui/react';
+import { Box, Button, Card, CardBody, CardHeader, Center, Heading, Text } from '@chakra-ui/react';
 import { InfoOutlineIcon } from '@chakra-ui/icons';
 import { useState } from 'react';
 
@@ -23,35 +23,55 @@ const testFlashcard: FlashcardContent[] = [
     title: "Masquerading",
     content: "creating a packet with arbitrary source address, packet content and destination address.\n \
     A receiver might be disguised of the true sender.",
-  }
+  },
+  {
+    id: 3, 
+    title: "Third for test",
+    content: "test third flashcard.",
+  },
 ];
 
 const Flashcard = () => {
+  const [count, setCount] = useState(1);
   const flashcards: FlashcardContent[] = testFlashcard;
   const [flashcard, setFlashcard] = useState(flashcards[0]);
   return (
-    <Center h="100vh" w="100vw"> 
-      <Box display="flex" flexDir="column">
-        <Card
-          alignSelf="center"
-          justifySelf="center"
-          maxW="lg"
-          bg="game.black"
-          borderColor="game.gray"
-          borderWidth="2px"
-        >
-          <CardHeader>
-            <Heading textColor="game.white">{flashcard.title}</Heading>
-          </CardHeader>
-          <CardBody>
-            <Box display="flex" gap="1em" alignItems="start">
-              <InfoOutlineIcon color="game.white" />
-              <Text color="game.white" maxW="sm">{flashcard.content}</Text>
-            </Box>
-          </CardBody>
-        </Card>
-      </Box>
-    </Center>
+    <>
+      <Center h="100vh" w="100vw"> 
+        <Box display="flex" flexDir="column" justifyContent="space-between" gap="4em">
+          <Card
+            alignSelf="center"
+            justifySelf="center"
+            maxW="lg"
+            bg="game.black"
+            borderColor="game.gray"
+            borderWidth="2px"
+          >
+            <CardHeader>
+              <Heading textColor="game.white">{flashcard.title}</Heading>
+            </CardHeader>
+            <CardBody>
+              <Box display="flex" gap="1em" alignItems="start">
+                <InfoOutlineIcon color="game.white" />
+                <Text color="game.white">{flashcard.content}</Text>
+              </Box>
+            </CardBody>
+          </Card>
+          <Button onClick={() => {
+            setCount(count + 1);
+            
+            if (count >= flashcards.length - 1) {
+              setCount(0) 
+            };
+            
+            setFlashcard(flashcards[count]);
+          }}
+          >
+            Next flashcard
+          </Button>
+        </Box>
+      </Center>
+    </>
   );
 };
 
