852d016f9a8af3247ac25ded89370436ac7439ef
kabizzle <kabirrbissessar@gmail.com>
2024-02-25 20:05:38 +0200

    extracted test flashcard data to separate file

--- src/components/Flashcards/Flashcard.tsx
+++ src/components/Flashcards/Flashcard.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Card, CardBody, CardHeader, Center, Heading, Text } from '
 import { ArrowBackIcon, InfoOutlineIcon } from '@chakra-ui/icons';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import * as FlashcardData from "./FlashcardData";
 
 interface FlashcardContent {
   id: number;
@@ -12,29 +13,9 @@ interface FlashcardContent {
 // FlashcardContent is the format for each flashcard.
 // Each flashcard should have a title and the content of the flashcard, along with an id to identify it.
 
-const testFlashcard: FlashcardContent[] = [
-  {
-    id: 1,
-    title: "Packet Sniffing",
-    content: "Definition: the practice of gathering, collecting, and logging packets that pass through a computer network.\n \
-    Due to security vulnerabilities, the content of packets can be inspected by malicious third-parties.",
-  },
-  {
-    id: 2, 
-    title: "Masquerading",
-    content: "creating a packet with arbitrary source address, packet content and destination address.\n \
-    A receiver might be disguised of the true sender.",
-  },
-  {
-    id: 3, 
-    title: "Third for test",
-    content: "test third flashcard.",
-  },
-];
-
 const Flashcard = () => {
   const [count, setCount] = useState(1);
-  const flashcards: FlashcardContent[] = testFlashcard;
+  const flashcards: FlashcardContent[] = FlashcardData.testFlashcards;
   const [flashcard, setFlashcard] = useState(flashcards[0]);
   return (
     <>
